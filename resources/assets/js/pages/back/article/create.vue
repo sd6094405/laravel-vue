a211a794cc6443be5d0ac4455bf837219eb676be
sd6094405 <hjhwq@126.com>
2019-01-07 19:05:12 +0800

    引入腾讯COS

--- resources/assets/js/pages/back/article/create.vue
+++ resources/assets/js/pages/back/article/create.vue
@@ -31,7 +31,7 @@
                     </el-form-item>
                 </el-form>
             </div>
-            <mavon-editor style="height:inherit" :ishljs="true" v-model="form.body"/>
+            <mavon-editor style="height:inherit" @imgAdd="$imgAdd" ishljs="true" :codeStyle="monokai-sublime" :ishljs="true" v-model="form.body"/>
             <div class="handle-box">
                 <el-button type="primary" @click="saveVisible = true">保存</el-button>
             </div>
@@ -52,6 +52,8 @@
 
 <script>
     import * as api from '../../../config/httpService'
+
+
 
     export default {
         data() {
@@ -107,6 +109,28 @@
                         this.$message.error('保存失败!');
                     });
             },
+
+            // 绑定@imgAdd event
+            $imgAdd(pos, $file){
+                // 第一步.将图片上传到服务器.
+                var formdata = new FormData();
+                formdata.append('image', $file);
+                console.log($file,pos);
+                axios({
+                    url: 'server url',
+                    method: 'post',
+                    data: formdata,
+                    headers: { 'Content-Type': 'multipart/form-data' },
+                }).then((url) => {
+                    // 第二步.将返回的url替换到文本原位置![...](0) -> ![...](url)
+                    /**
+                     * $vm 指为mavonEditor实例，可以通过如下两种方式获取
+                     * 1. 通过引入对象获取: `import {mavonEditor} from ...` 等方式引入后，`$vm`为`mavonEditor`
+                     * 2. 通过$refs获取: html声明ref : `<mavon-editor ref=md ></mavon-editor>，`$vm`为 `this.$refs.md`
+                     */
+                    $vm.$img2Url(pos, url);
+                })
+            }
 
         }
     }
