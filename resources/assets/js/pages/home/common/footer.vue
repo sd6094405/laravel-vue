0480852e5ee74f0bb5b186709a2378115dc7f202
hhb <547577437@qq.com>
2019-10-24 14:35:06 +0800

    hhb

--- resources/assets/js/pages/home/common/footer.vue
+++ resources/assets/js/pages/home/common/footer.vue
@@ -1,27 +1,33 @@
 <template>
-    <div>
-        <footer id="footer" role="contentinfo">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <p style="margin-top:15px;">
-                            <a href="https://www.hhbcode.cn" target="_blank"><strong>Powered by 咸鱼王</strong></a>
-                            &nbsp;&nbsp;
-                            <a href="https://github.com/sd6094405/laravel-vue" target="_blank">
-                                <span class="icon-github" style="padding-right:20px;"></span>
-                            </a>
-                        </p>
-                        <p>蜀ICP备19003103号</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
+  <div>
+    <footer id="footer" role="contentinfo">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <p style="margin-top:15px;">
+              <a href="https://www.hhbcode.cn" target="_blank">
+                <strong>Powered by 咸鱼王</strong>
+              </a>
+              &nbsp;&nbsp;
+              <a
+                href="https://github.com/sd6094405/laravel-vue"
+                target="_blank"
+              >
+                <span class="icon-github" style="padding-right:20px;"></span>
+              </a>
+            </p>
+            <a href="http://www.miitbeian.gov.cn">
+              <p>蜀ICP备19003103号</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-        }
-    }
+export default {
+  mounted() {}
+};
 </script>
